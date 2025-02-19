ff77bd1e33644edc5353bd2a0f51e1940ef09554
Pedro Alegria Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2025-02-18 22:07:07 -0300

    Update and rename main to App.vue

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      novaTarefa: "",
+      tarefas: []
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      if (this.novaTarefa.trim() !== "") {
+        this.tarefas.push({ texto: this.novaTarefa, concluida: false });
+        this.novaTarefa = "";
+      }
+    },
+    removerTarefa(index) {
+      this.tarefas.splice(index, 1);
+    },
+    alternarConclusao(index) {
+      this.tarefas[index].concluida = !this.tarefas[index].concluida;
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>📝 To-Do List</h1>
+
+    <div class="input-container">
+      <input v-model="novaTarefa" placeholder="Digite uma nova tarefa">
+      <button class="adicionar" @click="adicionarTarefa">➕</button>
+    </div>
+
+    <ul>
+      <li v-for="(tarefa, index) in tarefas" :key="index" class="tarefa">
+        <span :class="{ concluida: tarefa.concluida }" @click="alternarConclusao(index)">
+          {{ tarefa.texto }}
+        </span>
+        <div class="botoes">
+          <button class="remover" @click="removerTarefa(index)">❌</button>
+          <button class="finalizado" @click="alternarConclusao(index)">✅</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
